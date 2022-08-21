ab8432130ec66290c3a6c85c92a0eed5b914443f
ercutto <ercumentti81@gmail.com>
2022-08-21 21:55:12 +0300

    Clock for factory game
    I use local clock instead over the network more effective
    -next manage only from master client if time is over!

--- Assets/Game/Scripts/Gameplay/Mechanics/Game2 and 3/TogetherWinScore.cs
+++ Assets/Game/Scripts/Gameplay/Mechanics/Game2 and 3/TogetherWinScore.cs
@@ -8,23 +8,66 @@ namespace TwoPlayersGame
 {
     public class TogetherWinScore : MonoBehaviour,IPunObservable
     {
-        public Text score,WinMessageText;
+        public Text score,WinMessageText,ClockText;
         private int scoreValue=0;
         private string winMessage = "Awesome teamwork!";
         private string winMessageReset = "";
+        public string ClockTime;
+        public float timeCount = 0.0f;
+        public float Minutes = 0.0f;
+        public float GameTime = 3f;
+        public float seconds;
+        public bool begin;
         private PhotonView pv;
         // Start is called before the first frame update
         void Start()
         {
             pv = GetComponent<PhotonView>();
+            begin = true;
         }
 
         // Update is called once per frame
         void Update()
         {
+            //if (!PhotonNetwork.IsMasterClient)
+            //{
+            //    return;
+            //}
+            //else
+           // {
+                if (begin)
+                {
 
+                    Clock();
+                }
+                
+           // }
+
+            ClockText.text = ClockTime;
         }
-  
+       
+        void Clock()
+        {
+            timeCount += 1 * Time.deltaTime;
+            if (timeCount >= 1)
+            {
+                seconds++;
+                timeCount = 0.0f;
+                if (seconds >= 60)
+                {
+                    Minutes++;
+                    if (Minutes >= GameTime)
+                    {
+                        Debug.Log("Time is over!");
+                        begin = false;
+                        
+                    }
+                }
+            }
+            ClockTime = Minutes.ToString() + ":" + seconds.ToString() + ":" + timeCount.ToString("0,0");
+        }
+      
+
         public void AddScore(int add)
         {
             if (!PhotonNetwork.IsMasterClient)
@@ -73,11 +116,13 @@ namespace TwoPlayersGame
             {
                 stream.SendNext(scoreValue);
                 stream.SendNext(winMessage);
+                stream.SendNext(ClockTime);
             }
             else
             {
                 scoreValue = (int)stream.ReceiveNext();
                 winMessage = (string)stream.ReceiveNext();
+                ClockTime = (string)stream.ReceiveNext();
             }
         }
     }
