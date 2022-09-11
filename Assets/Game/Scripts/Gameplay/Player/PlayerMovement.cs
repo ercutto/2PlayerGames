25417c4bc13c4d4dfc459e984a2214d28a0ed9a0
ercutto <ercumentti81@gmail.com>
2022-09-11 11:47:12 +0300

    new Physicsmaterial avoiding players slide

--- Assets/Game/Scripts/Gameplay/Player/PlayerMovement.cs
+++ Assets/Game/Scripts/Gameplay/Player/PlayerMovement.cs
@@ -32,6 +32,7 @@ namespace TwoPlayersGame
         {
             if (pV.IsMine)
             {
+
                 if (SceneManagerHelper.ActiveSceneName == "Game" || SceneManagerHelper.ActiveSceneName == "Game 3" ||
                        SceneManagerHelper.ActiveSceneName == "Game 4" || SceneManagerHelper.ActiveSceneName == "Game 5")
                 {
@@ -61,31 +62,40 @@ namespace TwoPlayersGame
         #region Movements
         void GameOne()
         {
+         
+
             float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
             float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-            
             rb.AddForce(horizontal, 0, vertical,ForceMode.Force);
-            Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
-            if(movement!=Vector3.zero)
+            
+            Vector3 movement = new Vector3(horizontal, 0.0f, vertical).normalized;
+            if (movement!=Vector3.zero)
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), turnSpeed);
-            hand.transform.RotateAround(transform.position,new Vector3(0, 2, 0), 0);
+               hand.transform.RotateAround(transform.position,new Vector3(0, 2, 0), 0);
+            
             //PlayerGraphics.transform.rotation =Quaternion.Slerp(PlayerGraphics.transform.rotation,Quaternion.LookRotation(movement), turnSpeed);
 
         }
         void GameTwo()
         {
+            rb.transform.position = rb.transform.position;
+
             float horizontal = Input.GetAxis("Horizontal") * speed* 2 * Time.deltaTime;
  
             rb.AddForce(horizontal, 0, 0,ForceMode.Force);
+            Vector3 movement = new Vector3(horizontal, 0.0f, 0.0f).normalized;
 
             transform.rotation = Quaternion.Euler(0, horizontal * smallTilt, horizontal * smallTilt).normalized;
         }
         void GameSix()
         {
+            rb.transform.position = rb.transform.position;
             float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
             float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
             rb.AddForce(0, vertical, horizontal, ForceMode.Force);
+            Vector3 movement = new Vector3(0.0f,vertical,horizontal).normalized;
+            
             Quaternion newEulerAngle = Quaternion.Euler(0, 0, 20 + 10);
             transform.rotation = Quaternion.Euler(0,0, vertical * tilt).normalized;
             //Vector3 movement = new Vector3(horizontal, vertical, 0);
