65893417c575a2f267f395df7fb7170c7390b95a
ercutto <ercumentti81@gmail.com>
2022-10-16 17:47:42 +0300

    Fans
    Fans Are moving

---
+++ Assets/Game/Scripts/TestScripts/FansTest.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FansTest : MonoBehaviour
+{
+    public float yMax, yMin,speed,length;
+    public bool Goal;
+    public GameObject[] Fans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FansMove();
+
+
+    }
+    void FansMove()
+    { }
+ 
+}
